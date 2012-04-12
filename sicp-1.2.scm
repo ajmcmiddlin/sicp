bc8a7a98b86cc0789bf1596b2adff6f145939a3a
Andrew McCluskey <andrew@ajmccluskey.com>
2012-04-12 15:04:00 +1000

    sicp-1.2.scm: Added the answer for the first questions in 1.10

--- sicp-1.2.scm
+++ sicp-1.2.scm
@@ -45,3 +45,33 @@
 ; (second+ (dec 1) (inc 8))
 ; (second+ 0 9)
 ; 9
+
+
+; Exercise 1.10
+; ----------------------------------------
+(define (Ackermann x y)
+	(cond ((= y 0) 0)
+				((= x 0) (* 2 y))
+				((= y 1) 2)
+				(else (Ackermann (- x 1)
+												 (Ackermann x (- y 1))))))
+
+; (Ackermann 1 10)
+; (Ackermann 0 (Ackermann 1 9))
+; (Ackermann 0 (Ackermann 0 (Ackermann 1 8)))
+; ...
+; The pattern above continues so that each substitution results in another recursive call to Ackermann, where
+; the arguments are 0 and (Ackermann 1 (- y 1)) until y becomes 1.
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+; 
+; At this point, (Ackermann 1 1) returns 2, leaving us with the list of recursive calls below.
+; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+;
+; Each recursive call doubles the value of y in the previous call, which means that the final answer is
+; 2 x 2^n, where n is the number of calls; which is 2 x 2^9 = 2^10
+; 2^10
+; 1024
+
+
+
+
