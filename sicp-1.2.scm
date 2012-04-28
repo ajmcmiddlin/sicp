5d362ea29ce11669c99d8cf7edf0d8e3026714c4
Andrew McCluskey <andrew@ajmccluskey.com>
2012-04-28 22:02:24 +1000

    sicp-1.2: finished off exercise 1.10

--- sicp-1.2.scm
+++ sicp-1.2.scm
@@ -61,17 +61,48 @@
 ; (Ackermann 0 (Ackermann 0 (Ackermann 1 8)))
 ; ...
 ; The pattern above continues so that each substitution results in another recursive call to Ackermann, where
-; the arguments are 0 and (Ackermann 1 (- y 1)) until y becomes 1.
+; the arguments are 0 and (Ackermann 1 (- y 1)) until y becomes 1.  As n decrements by one each time we
+; perform a substitution, there will be (- n 1) recursive calls on the stack where 0 is the first argument,
+; and the second argument of the final call will be (A 1 1).
 ; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
 ; 
-; At this point, (Ackermann 1 1) returns 2, leaving us with the list of recursive calls below.
+; At this point, (Ackermann 1 1) returns 2, leaving us with the list of recursive calls below.  
 ; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
 ;
-; Each recursive call doubles the value of y in the previous call, which means that the final answer is
-; 2 x 2^n, where n is the number of calls; which is 2 x 2^9 = 2^10
-; 2^10
-; 1024
+; Each call to the Ackermann function where the first argument is 0 returns (* 2 y), therefore the list of
+; recursive calls has the effect of multiplying the second argument of the last call by 2^(n-1).  As the final
+; argument will always be 2 when the first argument of the original call is 1 (we add calls until we get to (A
+; 1 1)), any call to the Ackermann function that takes the form (A 1 n) will return 2^n.  Therefore the final
+; answer is 2^10 = 1024
 
 
+; (Ackermann 2 4)
+; (A 1 (A 2 3))
+; (A 1 (A 1 (A 2 2)))
+; (A 1 (A 1 (A 1 (A 2 1))))
+; (A 1 (A 1 (A 1 2)))
+; We know that each call to the Ackermann function that takes the form (A 1 n) equals 2^n, so we can progress
+; as follows
+; 2  ^  (2  ^ (2^2))
+; 2^(2^4)
+; 2^16 = 65536
 
 
+; (Ackermann 3 3)
+; (A 2 (A 3 2))
+; (A 2 (A 2 (A 3 1)))
+; (A 2 (A 2 2))
+; (A 2 (A 1 (A 2 1)))
+; (A 2 (A 1 2))
+; (A 2 4)
+; 65536
+
+
+; returns 2n
+; (define (f n) (Ackermann 0 n))
+;
+; returns 2^n
+; (define (g n) (Ackermann 1 n))
+;
+; returns 2^2^2... n times - e.g. (h 5) -> 2^2^2^2^2
+; (define (h n) (Ackermann 2 n))
