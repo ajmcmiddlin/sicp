a165f75463e2198bdcbed4a2faeafc7950b5cc68
Andrew McCluskey <andrew@ajmccluskey.com>
2012-06-10 17:05:26 +1000

    sicp-1.2.scm: exercise 1.16 solution was overly complicated
    For some silly reason, I forgot that simply re-expressing the solution when
    the exponent was even was working towards the base case.

--- sicp-1.2.scm
+++ sicp-1.2.scm
@@ -215,7 +215,7 @@
   (define (fast-expt-iter b n a)
     (cond ((= n 0) a)
           ((= n 1) (* a b))
-          ((even? n) (fast-expt-iter (* b b) (- (/ n 2) 1) (* a b b)))
+          ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
           (else (fast-expt-iter b (- n 1) (* a b)))))
 
   (fast-expt-iter b n 1))
